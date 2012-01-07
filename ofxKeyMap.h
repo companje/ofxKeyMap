2c501cd8e3036cb78131455b74579a385065cdc1
Rick Companje <rick@companje.nl>
2012-01-07 14:35:12 +0100

    Since OF007 this feature is part of OF.  You can now use ofGetKeyPressed(key) instead.

--- ofxKeyMap.h
+++ ofxKeyMap.h
@@ -1,36 +1,24 @@
-#ifndef OFX_KEY_MAP
-#define OFX_KEY_MAP
-
 /************************************************************
 
-OpenFrameworks Library
+OpenFrameworks addon
 
 File: 			ofxKeyMap.h
 Description: 	Keeps track of pressed keys
-Notes:
 
-Last Modified: 2009.09.17
-Creator: Rick Companje
+    Since OF007 this feature is part of OF.
+    You can now use ofGetKeyPressed(key) instead.
+
+By Rick Companje
+Created: 2009.09.17
+Last modified: 2011.12.16
 
 ************************************************************/
 
+#pragma once
 #include "ofMain.h"
-
-#define NUM_KEYS 512
 
 class ofxKeyMap {
 public:
-
-    ofxKeyMap();
-
-    bool isKeyDown(int key);
-
-    inline bool & operator [] (int i) { return keys[i]; }
-
-private:
-    bool keys[NUM_KEYS];
-    void keyPressed(ofKeyEventArgs &e);
-	void keyReleased(ofKeyEventArgs &e);
+    inline bool operator [] (int key) { return ofGetKeyPressed(key); }
+    
 };
-
-#endif
